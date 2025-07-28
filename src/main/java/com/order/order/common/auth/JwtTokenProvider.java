447dc2a2d384b0fb19f815ae499f49042f007ffa
Jihyeon0804 <cam080421@naver.com>
2025-07-28 18:01:21 +0900

    Jwt Token Provider

---
+++ src/main/java/com/order/order/common/auth/JwtTokenProvider.java
@@ -0,0 +1,59 @@
+package com.order.order.common.auth;
+
+import com.order.order.member.domain.Member;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${jwt.expirationAt}")
+    private int expirationAt;
+
+    @Value("${jwt.secretKeyAt}")
+    private String secretKeyAt;
+
+    private Key secret_at_key;
+
+    @PostConstruct
+    public void init() {
+        secret_at_key = new SecretKeySpec(java.util.Base64.getDecoder().decode(secretKeyAt)
+                , SignatureAlgorithm.HS512.getJcaName());
+    }
+
+    public String createAtToken(Member member) {
+        String email = member.getEmail();
+        String role = member.getRole().toString();
+        init();
+
+        Claims claims = Jwts.claims().setSubject(email);
+        claims.put("role", role);
+
+        Date now = new Date();
+        String token =Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + expirationAt * 90 * 1000L))
+                .signWith(secret_at_key)
+                .compact();
+
+        return token;
+    }
+
+    public String createRtToken(Member member) {
+        // 유효 기간이 긴 rt 토큰 생성
+        
+        
+        // rt 토큰을 redis에 저장
+
+        return null;
+    }
+}
