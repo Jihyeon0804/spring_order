72231936df673d938dba74f423a8c7db00210b4f
Jihyeon0804 <cam080421@naver.com>
2025-07-29 19:17:15 +0900

    Product

---
+++ src/main/java/com/order/order/product/domain/Product.java
@@ -0,0 +1,36 @@
+package com.order.order.product.domain;
+
+import com.order.order.common.domain.BaseTimeEntity;
+import com.order.order.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String category;
+    private int price;
+    private int stockQuantity;
+    private String imagePath;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    public void updateImageUrl(String imageUrl) {
+        this.imagePath = imageUrl;
+    }
+
+}
