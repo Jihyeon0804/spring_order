2ed2dde219d039cac42604ef6564e411a7ee130e
Jihyeon0804 <cam080421@naver.com>
2025-07-29 19:17:56 +0900

    Product Response DTO

---
+++ src/main/java/com/order/order/product/dto/ProductResDTO.java
@@ -0,0 +1,28 @@
+package com.order.order.product.dto;
+
+import com.order.order.product.domain.Product;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductResDTO {
+    private Long id;
+    private String name;
+    private String category;
+    private int price;
+    private int stockQuantity;
+
+    public static ProductResDTO fromEntity(Product product) {
+        return ProductResDTO.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .category(product.getCategory())
+                .price(product.getPrice())
+                .stockQuantity(product.getStockQuantity()).build();
+    }
+}
