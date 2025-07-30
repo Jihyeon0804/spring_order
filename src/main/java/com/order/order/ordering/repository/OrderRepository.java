2037845e69415d4fb4cc00150db67d03b7176509
Jihyeon0804 <cam080421@naver.com>
2025-07-30 17:47:58 +0900

    Order Repository

---
+++ src/main/java/com/order/order/ordering/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.order.order.ordering.repository;
+
+import com.order.order.ordering.domain.Ordering;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Ordering, Long> {
+
+}
