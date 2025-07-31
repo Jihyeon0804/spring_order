f7614b8885e4b2ad06af8caceb25997e8baa8ca5
Jihyeon0804 <cam080421@naver.com>
2025-07-31 17:47:19 +0900

    Order Repository

--- src/main/java/com/order/order/ordering/repository/OrderRepository.java
+++ src/main/java/com/order/order/ordering/repository/OrderRepository.java
@@ -1,10 +1,15 @@
 package com.order.order.ordering.repository;
 
+import com.order.order.member.domain.Member;
 import com.order.order.ordering.domain.Ordering;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Ordering, Long> {
+    List<Ordering> findAllByMemberId(Long id);
 
+    List<Ordering> findAllByMember(Member member);
 }
